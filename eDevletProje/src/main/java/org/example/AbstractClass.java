d0469b47388677fa3c5cab9bbb2d7dc02b25508e
buket.bektas <buketbkts13@gmail.com>
2024-05-22 17:22:30 +0300

    Abstract and InfoLog classes separated

--- eDevletProje/src/main/java/org/example/AbstractClass.java
+++ eDevletProje/src/main/java/org/example/AbstractClass.java
@@ -8,16 +8,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-class infoLog {
-    private static final  Logger LOGGER =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    public void sampleLog(String message) {
-
-        LOGGER.log(Level.INFO, message);
-    }
-}
 
 public abstract class AbstractClass {
 
